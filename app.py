a6e9cdc807dec1568ddae3ac4c59f2daaac7666a
Rasul0990 <rasulpalvanov007@gmail.com>
2025-05-05 22:14:49 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -129,4 +129,4 @@ def update_tabs(n_clicks, assets, keyword, date_range, tab):
 # Run App
 # ---------------------------
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=10000, debug=True)
